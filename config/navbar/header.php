80315cc1177a3216148a6566ed4244a7e98c5fd1
Gabriel Sevestedt <glissted@gmail.com>
2018-12-07 14:55:37 +0000

    Done with color analasys task.

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -15,23 +15,28 @@ return [
             "title" => "Första sidan, börja här.",
         ],
         [
+            "text" => "Rapport",
+            "url" => "rapport",
+            "title" => "rapport om andra hemidor.",
+        ],
+        [
             "text" => "Redovisning",
             "url" => "redovisning",
             "title" => "Redovisningstexter från kursmomenten.",
-            "submenu" => [
-                "items" => [
-                    [
-                        "text" => "Kmom01",
-                        "url" => "redovisning/kmom01",
-                        "title" => "Redovisning för kmom01.",
-                    ],
-                    [
-                        "text" => "Kmom02",
-                        "url" => "redovisning/kmom02",
-                        "title" => "Redovisning för kmom02.",
-                    ],
-                ],
-            ],
+            // "submenu" => [
+            //     "items" => [
+            //         [
+            //             "text" => "Kmom01",
+            //             "url" => "redovisning/kmom01",
+            //             "title" => "Redovisning för kmom01.",
+            //         ],
+            //         [
+            //             "text" => "Kmom02",
+            //             "url" => "redovisning/kmom02",
+            //             "title" => "Redovisning för kmom02.",
+            //         ],
+            //     ],
+            // ],
         ],
         [
             "text" => "Om",
